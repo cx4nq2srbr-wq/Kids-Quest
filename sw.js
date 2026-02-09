964338319ccbece932e4e13320879d59be30cba5
cx4nq2srbr-wq <cx4nq2srbr@privaterelay.appleid.com>
2026-02-09 00:38:29 -0700

    lets roll

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'quest-log-v1.0.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install the Service Worker and cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Serve files from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
